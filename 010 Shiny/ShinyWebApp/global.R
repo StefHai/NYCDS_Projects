8f45dec0637aa6b081158007e2d7ad1e3c586c44
StefHai <stefan.hainzer@gmail.com>
2017-07-19 00:23:03 -0400

    sha

--- 010 Shiny/ShinyWebApp/global.R
+++ 010 Shiny/ShinyWebApp/global.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-shares = read.csv("../double_listed_shares.csv", stringsAsFactors = F)
+shares = read.csv("double_listed_stocks.csv", stringsAsFactors = F)
 date.origin = '1970-01-01'
 shares$us_date = as.Date(shares$us_date, origin=date.origin)
 shares$in_date = as.Date(shares$in_date, origin=date.origin)
